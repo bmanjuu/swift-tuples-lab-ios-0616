4dfc6fdf6b09723f8a52f2b9f1321887fa30f34b
bmanjuu <bettyofung@gmail.com>
2017-01-28 10:58:58 -0500

    completed

--- Tuples.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ Tuples.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -15,7 +15,7 @@
  ### 1. Create a tuple to represent a person! The tuple should have a person's name in the first field and their age in the second field. Define the tuple as a variable named `person`.
  */
 // write your code here
-
+var person = (name: "Hermione", age: 26)
 
 
 
@@ -27,7 +27,7 @@
  ### 2. Print out "<Name> is <Age> years old" using the tuple you created in Question 1.
  */
 // write your code here
-
+print("\(person.name) is \(person.age) years old")
 
 
 
@@ -39,6 +39,7 @@
  ### 3. It is the person's birthday! Increment the person's age by one year that you created in Question 1.
  */
 // write your code here
+person.age = person.age + 1
 
 
 
@@ -51,7 +52,7 @@
  ### 4. Print out "Happy <Age> birthday, <Name>!" using the `person` variable.
  */
 // write your code here
-
+print("Happy \(person.age) birthday, \(person.name)!")
 
 
 
@@ -63,7 +64,7 @@
  ### 5. Create another person variable called `someone` using _named_ fields for the name and age.
  */
 // write your code here
-
+var anotherPerson = (name: "Ron", age: 28)
 
 
 
@@ -75,7 +76,7 @@
  ### 6. Print out "<Name> is <Age> years old" for the `someone` variable you created in Question 5.
  */
 // write your code here
-
+print("\(anotherPerson.name) is \(anotherPerson.age) years old")
 
 
 
@@ -86,7 +87,7 @@
  ### 7. It is `someone`'s birthday! Increment `someone`'s age by one year.
  */
 // write your code here
-
+anotherPerson.age += 1
 
 
 
@@ -97,7 +98,7 @@
  ### 8. Print out "Happy <Age> birthday, <Name>!" using the `someone` variable.
  */
 // write your code here
-
+print("Happy \(anotherPerson.age) birthday, \(anotherPerson.name)!")
 
 
 
@@ -109,7 +110,7 @@
  ### 9. Create a _constant_ tuple called `human`, with named fields for `name` and `age`.
  */
 // write your code here
-
+let human = (name: "Betty", age: 27)
 
 
 
@@ -120,7 +121,7 @@
 /*:
  ### 10. Can you change `human`'s age? Why or why not?
  */
-
+// no because human was declared as a constant
 
 
 
